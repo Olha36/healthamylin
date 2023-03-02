107f5c8aec7f542e9de728899e359ee6ab5e3ae0
Olha36 <olhakucheruk36@gmail.com>
2023-03-02 19:40:24 +0200

    add: text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import ResponsiveAppBar from './components/HeaderApp'
+import Text from './components/Text'
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
+      <Text />
     </div>
   )
 }
